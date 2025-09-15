7bf572ba0fbace9cbaa908838f0a9e492942bbc1
GeraldusGigih <gigihgeraldus@gmail.com>
2025-09-15 13:55:53 +0700

    id Fixed

--- main/models.py
+++ main/models.py
@@ -9,7 +9,6 @@ class Product(models.Model):
         ('equipment', 'Equipment'),
     ]
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     price = models.IntegerField()
     description = models.TextField()
