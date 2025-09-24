06b07dbd714e28b9884051fe24c4bbec8ff8b0a4
GeraldusGigih <gigihgeraldus@gmail.com>
2025-09-24 10:03:46 +0700

    Tugas 4: Bug Fix

--- main/views.py
+++ main/views.py
@@ -18,9 +18,9 @@ def show_main(request):
     filter_type = request.GET.get("filter", "all")  # default 'all'
 
     if filter_type == "all":
-        news_list = Product.objects.all()
+        product_list = Product.objects.all()
     else:
-        news_list = Product.objects.filter(user=request.user)
+        product_list = Product.objects.filter(user=request.user)
 
 
     context = {
